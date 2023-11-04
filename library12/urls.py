a2d399374811caedfa6d6a6b99901dca71e445d1
jiaYun516 <05162006keke@gmail.com>
2023-11-04 14:05:47 +0800

    1104

--- library12/urls.py
+++ library12/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from user_borrowAndReturn import views
 
 urlpatterns = [
-    path('/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',views.homepage),
     path('book/<int:id>/',views.searchById),
     path('category/<int:category_id>/',views.searchByCategory),
